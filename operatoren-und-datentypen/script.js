3ae69f23f651cb0b7a7e904c1e9b05a8e1b3f21b
Hamid Vahed <hamid.vahed75@gmail.com>
2023-08-16 14:08:30 +0200

    chore: The tasks are solved

---
+++ operatoren-und-datentypen/script.js
@@ -0,0 +1,31 @@
+// Write a String that logs out your first name
+console.log("Hamid");
+
+// Use the concatenation operator to concatenate each letter of your firstname into a string one letter by one.
+console.log("H" + "a" + "m" + "i" + "d");
+
+// Spot the problems & solve them
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 === 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+//Compare two conditions
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+
+console.log(25 / 5 === 50 / 10);
